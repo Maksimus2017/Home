acbef22f5b503b0ca95f71bbdcad8b1624116783
Maksim <rusant.k2017@yandex.ru>
2022-09-20 15:02:35 +0300

    solving

---
+++ solving/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] arr1 = new string[6] {"hello", "2", "Russia", "world", "ZOV","1922"};
+string[] arr2 = new string[arr1.Length];
+SecondArrayWithIF(arr1, arr2);
+PrintArray(arr2);
+void SecondArrayWithIF(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+    if(arr1[i].Length <= 3)
+        {
+        arr2[count] = arr1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
